387c5ad92e6f422fe93455961cc3e9f54b6c6353
kitamuraDev <masato1999824@icloud.com>
2022-08-27 22:36:08 +0900

    :sparkles: create DOM part

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
 const App = () => (
-  <div
-    style={{
-      backgroundColor: '#282c34',
-      color: 'white',
-      minHeight: '100vh',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      fontSize: 'calc(10px + 2vmin)',
-    }}
-  >
-    <p>Hello, React + TypeScript project Build with Vite.</p>
+  <div className='App'>
+    <div className='calculator'>
+      <div className='display'>
+        <span>0</span>0
+      </div>
+
+      <div className='operators'>
+        <button type='button'>/</button>
+        <button type='button'>*</button>
+        <button type='button'>+</button>
+        <button type='button'>-</button>
+        <button type='button'>DEL</button>
+      </div>
+
+      <div className='digits'>
+        {/* one ~ nien gen */}
+        {Array.from({ length: 9 }, (_, i) => (
+          <button key={i} type='button'>
+            {i + 1}
+          </button>
+        ))}
+        <button type='button'>0</button>
+        <button type='button'>.</button>
+        <button type='button'>=</button>
+      </div>
+    </div>
   </div>
 );
 
